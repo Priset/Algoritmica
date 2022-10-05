f58c13d111894d26aa9936c39f0e809245e5f3b8
Emanuel Lozano <90929324+Priset@users.noreply.github.com>
2022-10-04 20:35:12 -0400

    Add files via upload

--- Contest/F - Geppeto/f.cpp
+++ Contest/F - Geppeto/f.cpp
@@ -2,21 +2,39 @@
 #define input freopen("in.txt", "r", stdin)
 using namespace std;
 
- int main(){
+int status [21] [21], counter = 0;
+
+void fuerzaBruta (int k, int n, int arr[]) {
+    if (k == n){
+        vector<short> vec;
+        bool run = true;
+        for(int i = 0; i<n; i++)
+            if (arr[i]) vec.push_back(i + 1);
+
+        for (int i= 0; i < vec.size() && run; i++)
+            for (int j = i; j < vec.size() && run; j++)
+                if (status [vec [i]] [vec [j]]) {
+                    ++counter;
+                    run = false;
+                }
+    } else{
+        arr[k]=0;
+     fuerzaBruta (k + 1, n, arr);
+        arr[k]=1;
+     fuerzaBruta (k + 1, n, arr);
+    }
+}
+int main() {
     input;
-    int n,m; scanf("%d %d",&n,&m);
-    vector<pair<int, int> > v(m);
-    for (int i=0;i<m;i++) scanf("%d %d",&v[i].first,&v[i].second);
-
-    int res=0;
-    for (int mask=0;mask<(1<<n);mask++){
-        bool sw=true;
-        for (int i=0;i<v.size();i++){
-            if(mask&(1<<v[i].first) && mask&(1<<v[i].second)) sw=false;
-            res+=sw;
-            }
-        }
-    printf("%d\n", res);
-
-    return 0;
- }
+    int n, m;
+    cin>>n>>m;
+    int arr[20] = {0};
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin>>a>>b;
+        if (a > b) swap(a, b);
+        status[a][b] = 1;
+    }
+ fuerzaBruta (0, n, arr);
+    cout<<pow(2, n)-counter<<'\n';
+}
